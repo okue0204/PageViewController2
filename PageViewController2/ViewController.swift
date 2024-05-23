b2c54c01ef22665c81f118c0e9e9503c0eeebdc6
okue_hidetaka <okue@as-systems.jp>
2024-05-23 12:32:16 +0900

    firstCommit

---
+++ PageViewController2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageViewController2
+//
+//  Created by 奥江英隆 on 2024/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
